be8dce8fad2ad75d226db9ec3cf2ce696cfd9359
ABHIJITH P M <64658431+abhijithpm@users.noreply.github.com>
2025-06-26 09:44:10 +0530

    Fix Firebase Database Connection

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,4 +18,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
+
+// Initialize Analytics
 const analytics = getAnalytics(app);
+
+export { app, analytics };
