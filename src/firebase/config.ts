03fcbff72a9e559220d7c3dca652bd4220206fa3
ABHIJITH P M <64658431+abhijithpm@users.noreply.github.com>
2025-06-26 09:38:40 +0530

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,24 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, connectDatabaseEmulator } from 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBvOkBwRdtlAsAZzmPiIl13XhijOAWdTAg",
+  apiKey: "AIzaSyBryy_mbJ9qf-BBzYUHjgN9bpMxxhqUXk0",
   authDomain: "illam-mafia-demo.firebaseapp.com",
   databaseURL: "https://illam-mafia-demo-default-rtdb.firebaseio.com",
   projectId: "illam-mafia-demo",
-  storageBucket: "illam-mafia-demo.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:0123456789abcdef"
+  storageBucket: "illam-mafia-demo.firebasestorage.app",
+  messagingSenderId: "243599823200",
+  appId: "1:243599823200:web:455287bfc1c2aca21b8e05",
+  measurementId: "G-GT7XJWN0LN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-export const database = getDatabase(app);
-
-// Enable offline persistence
-// database.goOffline();
-// database.goOnline();
-
-export default app;
+const analytics = getAnalytics(app);
